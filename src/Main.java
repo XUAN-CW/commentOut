820fe98065dcc80ac6b9b5728af9d66580eea8f9
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2020-10-03 11:46:33 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,29 +5,34 @@ import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/**
- * 经验：
- * * 对文件和文件夹命名不能使用以下9个字符：
- *   / \ : * " < >  ？
- *   否则会导致 File.mkdir() 失败
- */
-
 public class Main {
 	public static void main(String[] args) throws Exception {
-		GetFile getFile = new GetFile(new File("C:\\Users\\86188\\Desktop\\learnSpringFromItheima\\copy"));
+		GetFile getFile = new GetFile(new File("D:\\core\\Java\\framework\\learnSpringFromItheima\\copy\\03_itheima_spring_aop\\itheima_spring_aop\\src"));
 		for (File file:getFile.getFiles(getFile.selectNameContains(""))) {
+			if (file.getParentFile().getName().equals(".idea")){
+				continue;
+			}
+			if (file.getName().substring(file.getName().length()-6).equals(".class")){
+				continue;
+			}
+
 			if (file.getName().substring(file.getName().length()-5).equals(".java")){
-				System.out.println(file.getName());
+				System.out.println("注释了："+file.getName());
 				SaveAndRead.save(file.getAbsolutePath(),comment(file.getAbsolutePath(),"//",""));
 			}
-			if (file.getName().substring(file.getName().length()-4).equals(".xml")){
-				System.out.println(file.getName());
+			else if (file.getName().substring(file.getName().length()-4).equals(".xml")){
+				System.out.println("注释了："+file.getName());
 				SaveAndRead.save(file.getAbsolutePath(),comment(file.getAbsolutePath(),"<!--","-->"));
 			}
-			if (file.getName().substring(file.getName().length()-4).equals(".jsp")){
-				System.out.println(file.getName());
+			else if (file.getName().substring(file.getName().length()-4).equals(".jsp")){
+				System.out.println("注释了："+file.getName());
 				SaveAndRead.save(file.getAbsolutePath(),comment(file.getAbsolutePath(),"<%--","--%>"));
 			}
+			else if (file.getName().substring(file.getName().length()-11).equals(".properties")){
+				System.out.println("注释了："+file.getName());
+				SaveAndRead.save(file.getAbsolutePath(),comment(file.getAbsolutePath(),"#",""));
+			}
+
 		}
 	}
 
